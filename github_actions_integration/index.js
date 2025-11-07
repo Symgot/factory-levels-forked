acfd280c07e89a35fc9dd2d58593e77f787a4d1c
Symgot <hendrik.wehlen@outlook.de>
2025-11-07 18:21:59 +0100

    Delete API-Integration

--- github_actions_integration/index.js
+++
@@ -1,9 +0,0 @@
-const WorkflowDispatcher = require('./workflow_dispatcher');
-const RunnerCoordinator = require('./runner_coordinator');
-const StatusTracker = require('./status_tracker');
-
-module.exports = {
-  WorkflowDispatcher,
-  RunnerCoordinator,
-  StatusTracker
-};
